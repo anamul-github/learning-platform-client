b7a47acdf9c154761e8612719ff53b6073adbbcf
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-24 18:37:41 -0400

    changed title name, made a folder for routes, imported RouteProvider, send route data, added a layout folder and a Main.js file inside that folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Welcome to My Learning Hour Client Side</h2>
+    <div>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
