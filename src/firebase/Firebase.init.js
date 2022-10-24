f239594daa67ca407628626eff0b58679ffa7530
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-24 14:19:23 -0400

    installed react router dom, firebase, added firebase configuration indie Firebase.init, and export default app

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyClfuYSxpIS9JncAhFOehdU0prV6xJQvkU",
+    authDomain: "learning-hour-68635.firebaseapp.com",
+    projectId: "learning-hour-68635",
+    storageBucket: "learning-hour-68635.appspot.com",
+    messagingSenderId: "557240193272",
+    appId: "1:557240193272:web:2cfaca3434f65cf8226b0d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
