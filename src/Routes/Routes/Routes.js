d42d3abaa01a5fcc2fd81835f6c2cf28e8af2c3a
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-24 20:50:49 -0400

    created lots of components, created routes, shared folder for some routes, implemented react-bootstrap, added left side nav and imported outlet in main file

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,12 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog/Blog";
+import Categories from "../../Pages/Categories/Categories/Categories";
+import FAQ from "../../Pages/FAQ/FAQ/FAQ";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Register/Register/Register";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
         children: [
-
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/categories',
+                element: <Categories></Categories>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
         ]
     }
 ])
