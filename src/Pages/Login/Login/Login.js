513af3f430337902b0f8358327452eb7a04fd8bf
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-26 02:25:08 -0400

    added login form and implemented google login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
+
+    const { providerLogin } = useContext(AuthContext);
+
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    const { user } = useContext(AuthContext);
+
     return (
-        <div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            <br />
+            <br />
             <ButtonGroup vertical>
-                <Button variant="outline-primary">Login with Google</Button>
+                <Button onClick={handleGoogleSignIn} variant="outline-primary">Login with Google</Button>
                 <Button variant="outline-secondary">Login with Github</Button>
             </ButtonGroup>
-        </div>
+        </Form>
     );
 };
 
